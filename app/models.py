4f110b1f9b1de586d2a22ae9a5224aaefa65b9ee
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2024-04-04 22:21:06 +0530

    implemented comment, share and like models

--- app/models.py
+++ app/models.py
@@ -48,3 +48,51 @@ class User:
         }
 
 
+class Comment:
+    def __init__(self, user_id, reel_id, text):
+        self.user_id = user_id
+        self.reel_id = reel_id
+        self.text = text
+
+    def save(self):
+        mongo.db.comments.insert_one({
+            'user_id': self.user_id,
+            'reel_id': self.reel_id,
+            'text': self.text
+        })
+
+    @staticmethod
+    def find_by_reel_id(reel_id):
+        return mongo.db.comments.find({'reel_id': reel_id})
+
+
+class Share:
+    def __init__(self, user_id, reel_id):
+        self.user_id = user_id
+        self.reel_id = reel_id
+
+    def save(self):
+        mongo.db.shares.insert_one({
+            'user_id': self.user_id,
+            'reel_id': self.reel_id
+        })
+
+    @staticmethod
+    def find_by_user_and_reel(user_id, reel_id):
+        return mongo.db.shares.find_one({'user_id': user_id, 'reel_id': reel_id})
+
+
+class Like:
+    def __init__(self, user_id, reel_id):
+        self.user_id = user_id
+        self.reel_id = reel_id
+
+    def save(self):
+        mongo.db.likes.insert_one({
+            'user_id': self.user_id,
+            'reel_id': self.reel_id
+        })
+
+    @staticmethod
+    def find_by_user_and_reel(user_id, reel_id):
+        return mongo.db.likes.find_one({'user_id': user_id, 'reel_id': reel_id})
