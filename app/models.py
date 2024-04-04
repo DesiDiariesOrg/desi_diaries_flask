20c1b7d1ebde32ba9708748190dd17f20d66f8f7
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2024-04-04 22:22:45 +0530

    notificatios model added

--- app/models.py
+++ app/models.py
@@ -1,4 +1,8 @@
 from app import mongo, bcrypt
+import boto3
+from bson import ObjectId
+from config import Config
+from botocore.exceptions import NoCredentialsError
 
 class User:
     def __init__(self, username, email, password, profile_pic=None, banner_pic=None, role='user', bookmarks=None):
@@ -80,6 +84,24 @@ class Share:
     @staticmethod
     def find_by_user_and_reel(user_id, reel_id):
         return mongo.db.shares.find_one({'user_id': user_id, 'reel_id': reel_id})
+
+class Notification:
+    def __init__(self, user_id, type, action_id):
+        self.user_id = user_id
+        self.type = type  # Type of notification (e.g., 'comment', 'like', 'reply')
+        self.action_id = action_id  # ID of the action triggering the notification
+
+    def save(self):
+        mongo.db.notifications.insert_one({
+            'user_id': self.user_id,
+            'type': self.type,
+            'action_id': self.action_id,
+            'read': False  # Flag to mark whether the notification has been read
+        })
+
+    @staticmethod
+    def find_by_user_id(user_id):
+        return mongo.db.notifications.find({'user_id': user_id})
 
 
 class Like:
