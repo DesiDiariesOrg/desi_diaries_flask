0d418dfd10fd9ff3870a193854ec2eef995b2467
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2024-04-12 16:58:17 +0530

    moved admin specific routes from categories to admin model

--- app/controllers/admin_controller.py
+++ app/controllers/admin_controller.py
@@ -1,4 +1,5 @@
-from flask import jsonify
+from flask import jsonify, request
+from app.models import Category
 from flask_login import current_user
 from app import mongo
 from bson import ObjectId
@@ -18,3 +19,21 @@ def delete_user(user_id):
     mongo.db.users.delete_one({'_id': ObjectId(user_id)})
     return jsonify({'message': 'User deleted successfully'}), 200
 
+def create_category():
+    if current_user.role != 'admin':
+        return jsonify({'error': 'Unauthorized access'}), 403
+
+    data = request.json
+    title = data.get('title')
+    thumbnail = data.get('thumbnail')
+
+    new_category = Category(title, thumbnail)
+    new_category.save()
+    return jsonify({'message': 'Category created successfully'}), 201
+
+def delete_category(category_id):
+    if current_user.role != 'admin':
+        return jsonify({'error': 'Unauthorized access'}), 403
+
+    Category.delete_by_id(category_id)
+    return jsonify({'message': 'Category deleted successfully'}), 200
--- app/controllers/category_controller.py
+++ app/controllers/category_controller.py
@@ -1,25 +1,5 @@
-from flask import request, jsonify
-from flask_login import current_user
+from flask import jsonify
 from app.models import Category
-
-def create_category():
-    if current_user.role != 'admin':
-        return jsonify({'error': 'Unauthorized access'}), 403
-
-    data = request.json
-    title = data.get('title')
-    thumbnail = data.get('thumbnail')
-
-    new_category = Category(title, thumbnail)
-    new_category.save()
-    return jsonify({'message': 'Category created successfully'}), 201
-
-def delete_category(category_id):
-    if current_user.role != 'admin':
-        return jsonify({'error': 'Unauthorized access'}), 403
-
-    Category.delete_by_id(category_id)
-    return jsonify({'message': 'Category deleted successfully'}), 200
 
 def get_categories():
     categories = Category.find_all()
